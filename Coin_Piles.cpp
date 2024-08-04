c189b430257acc8351cbdeab5c1417d64d31ed02
Nihat Garibli <150590673+nihatgaribli@users.noreply.github.com>
2024-08-04 15:58:17 +0400

    Update Coin_Piles.cpp

--- Coin_Piles.cpp
+++ Coin_Piles.cpp
@@ -5,12 +5,21 @@ int main()
 {
     int n;
     cin >> n;
-    vector<pair<int,int>> input(n);
+    vector<pair<int, int>> input(n);
     for (int i = 0; i < n; ++i)
     {
         cin >> input[i].first >> input[i].second;
     }
 
-    
-
+    for (int i = 0; i < n; i++)
+    {
+        if ((2 * input[i].first - input[i].second) % 3 || (2 * input[i].first - input[i].second) < 0 || (2 * input[i].second - input[i].first) % 3 || (2 * input[i].second - input[i].first) < 0)
+        {
+            cout << "NO\n";
+        }
+        else
+        {
+            cout << "YES\n";
+        }
+    }
 }
